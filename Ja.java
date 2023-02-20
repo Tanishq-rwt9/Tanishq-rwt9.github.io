3f5eaf055bb2a1d991a4c00fc9f7680207155d88
Tanishq Rawat <75470183+Tanishq-rwt9@users.noreply.github.com>
2023-02-20 15:53:41 +0530

    Create Ja.java

---
+++ Ja.java
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '9385e395ffb9efa0b4edc6b0be746bb7',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/9385e395ffb9efa0b4edc6b0be746bb7/invoke.js"></scr' + 'ipt>');
+</script>
